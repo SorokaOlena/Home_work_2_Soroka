ab88b2a521625e350dd653a73b3afcc9c4c35c20
Olena <sorotschenja@gmail.com>
2019-06-10 01:58:46 +0300

    Commit 2 - Add the program

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+public static void main(String[] args) {
+        //1.Створила maven проект, додала dependency selenium і файл .gitignore і запушила
+        //2.Створила програму і запушила в нову гілку
+        String s= "Hello=)";
+        System.out.println(s);
+        }
+        }
